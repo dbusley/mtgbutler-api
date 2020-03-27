9c3efd24c01781d1ca130e528bad351427f379f8
Doug Busley <dougbusley@gmail.com>
2020-03-26 21:19:19 -0700

    removed post/put/delete methods

--- src/main/java/com/mtgbutler/api/repository/CardRepository.java
+++ src/main/java/com/mtgbutler/api/repository/CardRepository.java
@@ -10,6 +10,7 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Optional;
 
@@ -17,8 +18,31 @@ public interface CardRepository
     extends PagingAndSortingRepository<Card, Long>,
         QuerydslPredicateExecutor<Card>,
         QuerydslBinderCustomizer<QCard> {
+    @Override
+    @RestResource(exported = false)
+    <S extends Card> S save(S s);
 
-  @Override
+    @Override
+    @RestResource(exported = false)
+    <S extends Card> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Long aLong);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Card card);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Card> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
+
+    @Override
   default void customize(QuerydslBindings bindings, QCard root) {
     bindings.bind(root.name).first(StringExpression::startsWithIgnoreCase);
 
--- src/main/java/com/mtgbutler/api/repository/PriceRepository.java
+++ src/main/java/com/mtgbutler/api/repository/PriceRepository.java
@@ -2,5 +2,31 @@ package com.mtgbutler.api.repository;
 
 import com.mtgbutler.api.model.Price;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-public interface PriceRepository extends PagingAndSortingRepository<Price, Long> {}
+public interface PriceRepository extends PagingAndSortingRepository<Price, Long> {
+    @Override
+    @RestResource(exported = false)
+    <S extends Price> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Price> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Long aLong);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Price price);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Price> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
+}
