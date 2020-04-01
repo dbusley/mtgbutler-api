bad91337bb933c9cc4e9666f9efe370fcbfd8847
Doug Busley <dougbusley@gmail.com>
2020-03-31 17:15:58 -0700

    auth fix

--- src/main/java/com/mtgbutler/api/configuration/SecurityConfiguration.java
+++ src/main/java/com/mtgbutler/api/configuration/SecurityConfiguration.java
@@ -17,7 +17,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().anyRequest().permitAll().and().httpBasic().and().csrf().disable();
+    http.authorizeRequests().anyRequest().permitAll();
   }
 
   @Override
